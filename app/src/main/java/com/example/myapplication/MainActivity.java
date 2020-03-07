53fb2c32a4a0a4f99277047e947161d61f662a27
kapis18 <kapis1881@gmail.com>
2020-03-07 22:01:16 +0100

    basic operations handling added (still many bugs)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,8 +9,123 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    Operation currentOperation = Operation.NONE;
     private TextView onScreenValueView;
-    private boolean blockOperations = false;
+    private double firstOperand = 0.0;
+    private double secondOperand = 0.0;
+
+    @Override
+    public void onClick(View v) {
+        String resName = getResources().getResourceEntryName(v.getId());
+        if (onScreenValueView.getText().length() > 9 && Character.isDigit(resName.charAt(resName.length() - 1))) { //restrict input screen to 10 characters
+            return;
+        }
+        if (currentOperation != Operation.NONE && Character.isDigit(resName.charAt(resName.length() - 1))) {
+            onScreenValueView.setText("");
+        }
+        switch (v.getId()) {
+            case R.id.button_0:
+                onScreenValueView.append("0");
+                break;
+            case R.id.button_1:
+                onScreenValueView.append("1");
+                break;
+            case R.id.button_2:
+                onScreenValueView.append("2");
+                break;
+            case R.id.button_3:
+                onScreenValueView.append("3");
+                break;
+            case R.id.button_4:
+                onScreenValueView.append("4");
+                break;
+            case R.id.button_5:
+                onScreenValueView.append("5");
+                break;
+            case R.id.button_6:
+                onScreenValueView.append("6");
+                break;
+            case R.id.button_7:
+                onScreenValueView.append("7");
+                break;
+            case R.id.button_8:
+                onScreenValueView.append("8");
+                break;
+            case R.id.button_9:
+                onScreenValueView.append("9");
+                break;
+            case R.id.button_x:
+                if (onScreenValueView.getText().length() == 0) {
+                    break;
+                }
+                currentOperation = Operation.MULTIPLY;
+                firstOperand = Double.parseDouble(onScreenValueView.getText().toString());
+                break;
+            case R.id.button_plus:
+                if (onScreenValueView.getText().length() == 0) {
+                    break;
+                }
+                currentOperation = Operation.ADD;
+                firstOperand = Double.parseDouble(onScreenValueView.getText().toString());
+                break;
+            case R.id.button_minus:
+                if (onScreenValueView.getText().length() == 0) {
+                    break;
+                }
+                currentOperation = Operation.SUBTRACT;
+                firstOperand = Double.parseDouble(onScreenValueView.getText().toString());
+                break;
+            case R.id.button_division:
+                if (onScreenValueView.getText().length() == 0) {
+                    break;
+                }
+                currentOperation = Operation.DIVIDE;
+                firstOperand = Double.parseDouble(onScreenValueView.getText().toString());
+                break;
+            case R.id.button_dot:
+                if (!onScreenValueView.getText().toString().contains(".")) {        //only one dot at a time is allowed in an expression
+                    onScreenValueView.append(".");
+                }
+                break;
+            case R.id.button_clear:
+                onScreenValueView.setText("");
+                currentOperation = Operation.NONE;
+                break;
+            case R.id.button_back:
+                String current = onScreenValueView.getText().toString();
+                if (current.length() > 0) {
+                    onScreenValueView.setText(current.substring(0, current.length() - 1));
+                }
+                break;
+            case R.id.button_equals:
+                if (currentOperation != Operation.NONE) {
+                    secondOperand = Double.parseDouble(onScreenValueView.getText().toString());
+                }
+                switch (currentOperation) {
+                    case NONE:
+                        break;
+                    case ADD:
+                        onScreenValueView.setText(String.valueOf(firstOperand + secondOperand));
+                        break;
+                    case SUBTRACT:
+                        onScreenValueView.setText(String.valueOf(firstOperand - secondOperand));
+                        break;
+                    case MULTIPLY:
+                        onScreenValueView.setText(String.valueOf(firstOperand * secondOperand));
+                        break;
+                    case DIVIDE:
+                        if (secondOperand != 0) {
+                            onScreenValueView.setText(String.valueOf(firstOperand / secondOperand));
+                        } else
+                            onScreenValueView.setText("ERROR");
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,72 +190,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
 
-    @Override
-    public void onClick(View v) {
-        String resName = getResources().getResourceEntryName(v.getId());
-        if (onScreenValueView.getText().length() > 9 && Character.isDigit(resName.charAt(resName.length() - 1))) {
-            return;
-        }
-        switch (v.getId()) {
-            case R.id.button_0:
-                onScreenValueView.append("0");
-                break;
-            case R.id.button_1:
-                onScreenValueView.append("1");
-                break;
-            case R.id.button_2:
-                onScreenValueView.append("2");
-                break;
-            case R.id.button_3:
-                onScreenValueView.append("3");
-                break;
-            case R.id.button_4:
-                onScreenValueView.append("4");
-                break;
-            case R.id.button_5:
-                onScreenValueView.append("5");
-                break;
-            case R.id.button_6:
-                onScreenValueView.append("6");
-                break;
-            case R.id.button_7:
-                onScreenValueView.append("7");
-                break;
-            case R.id.button_8:
-                onScreenValueView.append("8");
-                break;
-            case R.id.button_9:
-                onScreenValueView.append("9");
-                break;
-            case R.id.button_x:
-                //onScreenValueView.append("x");
-                break;
-            case R.id.button_plus:
-                //onScreenValueView.append("+");
-                break;
-            case R.id.button_minus:
-                //onScreenValueView.append("-");
-                break;
-            case R.id.button_division:
-                //onScreenValueView.append("÷");
-                break;
-            case R.id.button_dot:
-                onScreenValueView.append(".");
-                break;
-            case R.id.button_clear:
-                onScreenValueView.setText("");
-                break;
-            case R.id.button_back:
-                String current = onScreenValueView.getText().toString();
-                if (current.length() > 0) {
-                    onScreenValueView.setText(current.substring(0, current.length() - 1));
-                }
-                break;
-            case R.id.button_equals:
-                //onScreenValueView.setText("");
-                break;
-            default:
-                break;
-        }
+    enum Operation {
+        NONE,
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE
     }
 }
