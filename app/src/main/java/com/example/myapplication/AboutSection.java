8a7c49d36392571bbfcf9d2adf16c1cafbdd9966
kapis18 <kapis1881@gmail.com>
2020-04-05 14:57:15 +0200

    About section now properly implemented

--- app/src/main/java/com/example/myapplication/AboutSection.java
+++ app/src/main/java/com/example/myapplication/AboutSection.java
@@ -1,7 +1,16 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AboutSection extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.about_layout);
+    }
 }
