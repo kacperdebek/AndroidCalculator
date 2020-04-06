280bdbf5d59ac246243ddbcd78734ae4ebf2b3d2
kapis18 <kapis1881@gmail.com>
2020-04-06 14:00:35 +0200

    Added some more error handling and fixed character length limiting

--- app/src/main/java/com/example/myapplication/CalculatorUtills.java
+++ app/src/main/java/com/example/myapplication/CalculatorUtills.java
@@ -6,9 +6,11 @@ class CalculatorUtills {
         if (output.contains("E")) {
             int index = output.indexOf("E");
             int exponentLength = output.length() - index;
-            newOutput = output.substring(0, 10 - exponentLength) + output.substring(index);
+            newOutput = output.length() > 10
+                    ? output.substring(0, 10 - exponentLength) + output.substring(index)
+                    : output.substring(0, output.length() - exponentLength) + output.substring(index);
         } else {
-            newOutput = output.substring(0, 10);
+            newOutput = output.length() > 10 ? output.substring(0, 10) : output;
         }
         return newOutput;
     }
